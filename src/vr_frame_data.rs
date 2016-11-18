10959c1b6e26e00ba1a538d7f05697f922795777
Imanol Fernandez <mortimergoro@gmail.com>
2016-11-18 16:20:55 +0100

    Transmute VRFrameData to bytes to implement forward serialization in ipc-channels

--- src/vr_frame_data.rs
+++ src/vr_frame_data.rs
@@ -1,4 +1,6 @@
 use VRPose;
+use std::mem;
+use std::ptr;
 
 // Represents all the information needed to render a single frame of a VR scene
 #[allow(unused_attributes)]
@@ -9,16 +11,16 @@ pub struct VRFrameData {
     pub timestamp: u64,
 
     // major order column matrix describing the projection to be used for the left eye’s rendering
-    pub left_projection_matrix: [f32; 16], 
+    pub left_projection_matrix: [f32; 16],
 
     // major order column matrix describing the view transform to be used for the left eye’s rendering
-    pub left_view_matrix: [f32; 16], 
+    pub left_view_matrix: [f32; 16],
 
     // major order column matrix describing the projection to be used for the right eye’s rendering
-    pub right_projection_matrix: [f32; 16], 
+    pub right_projection_matrix: [f32; 16],
 
     // major order column matrix describing the view transform to be used for the right eye’s rendering
-    pub right_view_matrix: [f32; 16], 
+    pub right_view_matrix: [f32; 16],
  
     // VRPose containing the future predicted pose of the VRDisplay
     // when the current frame will be presented.
@@ -34,6 +36,29 @@ impl Default for VRFrameData {
             right_projection_matrix: identity_matrix!(),
             right_view_matrix: identity_matrix!(),
             pose: VRPose::default(),
+        }
+    }
+}
+
+impl VRFrameData {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut vec = vec![0u8; mem::size_of::<VRFrameData>()];
+        unsafe {
+            ptr::copy_nonoverlapping(mem::transmute(self),
+                                    vec.as_mut_ptr(),
+                                    mem::size_of::<VRFrameData>());
+        }
+        vec
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> VRFrameData {
+        unsafe {
+            let mut result: VRFrameData = mem::uninitialized();
+            ptr::copy_nonoverlapping(bytes.as_ptr(),
+                                     mem::transmute(&mut result),
+                                     mem::size_of::<VRFrameData>());
+
+            result        
         }
     }
 }

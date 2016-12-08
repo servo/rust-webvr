c4ca3ae9256fa8987dd0ca19122028b9f0936c53
Imanol Fernandez <mortimergoro@gmail.com>
2016-12-08 18:25:44 +0100

    add missinf files

---
+++ src/vr_gamepad.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+use std::cell::RefCell;
+use VRPose;
+
+pub type VRGamepadPtr = Arc<RefCell<VRGamepad>>;
+
+pub trait VRGamepad {
+    fn id(&self) -> u64;
+    fn name(&self) -> String;
+    fn state(&self) -> VRGamepadState;
+}
+
+#[allow(unused_attributes)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct VRGamepadState {
+    pub connected: bool,
+    pub timestamp: f64,
+    pub axes: Vec<f64>,
+    pub buttons: Vec<VRGamepadButton>,
+    pub pose: VRPose
+}
+
+impl Default for VRGamepadState {
+     fn default() -> VRGamepadState {
+         VRGamepadState {
+            connected: false,
+            timestamp: 0.0,
+            axes: Vec::new(),
+            buttons: Vec::new(),
+            pose: VRPose::default()
+         }
+     }
+}
+
+#[allow(unused_attributes)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub struct VRGamepadButton {
+    pub pressed: bool,
+    pub touched: bool
+}

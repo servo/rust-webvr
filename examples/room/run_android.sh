4d18f6e8dbe2ab72932831f18a51ec10184c8de3
Imanol Fernandez <mortimergoro@gmail.com>
2017-06-09 21:16:26 +0200

    Add Gear VR flavor to room scale sample

--- examples/room/run_android.sh
+++ examples/room/run_android.sh
@@ -2,5 +2,6 @@
 set -e
 cargo build --target=arm-linux-androideabi --release
 cd ./android
-./gradlew appStart 
-adb logcat
+./gradlew installGearvrArmRelease
+adb shell am start -n com.rust.webvr/com.rust.webvr.MainActivity
+#logcat-color | egrep '(RustAndroid|art|DEBUG)'

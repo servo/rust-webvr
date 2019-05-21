caa32ec68ae1b359bb08ebc3ad8fb80a23b48fc2
Manish Goregaokar <manishsmail@gmail.com>
2019-05-20 17:55:51 -0700

    Get frame and display data from MockVRState

--- rust-webvr/src/api/mock/display.rs
+++ rust-webvr/src/api/mock/display.rs
@@ -13,18 +13,21 @@ pub struct MockVRDisplay {
     state: Arc<Mutex<MockVRState>>,
 }
 
-#[derive(Default)]
-pub struct MockVRState {}
+pub struct MockVRState {
+    display_data: VRDisplayData,
+    frame_data: VRFrameData,
+}
 
 unsafe impl Send for MockVRDisplay {}
 unsafe impl Sync for MockVRDisplay {}
 
 impl MockVRDisplay {
     pub fn new() -> MockVRDisplayPtr {
+        let display_id = utils::new_id();
         Arc::new(RefCell::new(MockVRDisplay {
-            display_id: utils::new_id(),
+            display_id,
             attributes: Default::default(),
-            state: Default::default(),
+            state: Arc::new(Mutex::new(MockVRState::new(display_id))),
         }))
     }
 
@@ -40,78 +43,11 @@ impl VRDisplay for MockVRDisplay {
     }
 
     fn data(&self) -> VRDisplayData {
-        let mut data = VRDisplayData::default();
-        
-        // Mock display data
-        // Simulates a virtual HTC Vive
-
-        data.display_name = "Mock VRDisplay".into();
-        data.display_id = self.display_id;
-        data.connected = true;
-
-        data.capabilities.can_present = true;
-        data.capabilities.has_orientation = true;
-        data.capabilities.has_external_display = true;
-        data.capabilities.has_position = true;
-
-        data.stage_parameters = Some(VRStageParameters {
-            sitting_to_standing_transform: [-0.9317312, 0.0, 0.36314875, 0.0, 0.0, 0.99999994, 0.0, 0.0, -0.36314875, 
-                                            0.0, -0.9317312, 0.0, 0.23767996, 1.6813644, 0.45370483, 1.0],
-            size_x: 2.0,
-            size_z: 2.0
-        });
-
-        data.left_eye_parameters.offset = [0.035949998, 0.0, 0.015];
-        data.left_eye_parameters.render_width = 1512;
-        data.left_eye_parameters.render_height = 1680;
-        data.left_eye_parameters.field_of_view.up_degrees = 55.82093048095703;
-        data.left_eye_parameters.field_of_view.right_degrees = 51.26948547363281;
-        data.left_eye_parameters.field_of_view.down_degrees = 55.707801818847656;
-        data.left_eye_parameters.field_of_view.left_degrees = 54.42263412475586;
-
-        data.right_eye_parameters.offset = [-0.035949998, 0.0, 0.015];
-        data.right_eye_parameters.render_width = 1512;
-        data.right_eye_parameters.render_height = 1680;
-        data.right_eye_parameters.field_of_view.up_degrees = 55.898048400878906;
-        data.right_eye_parameters.field_of_view.right_degrees = 54.37410354614258;
-        data.right_eye_parameters.field_of_view.down_degrees = 55.614715576171875;
-        data.right_eye_parameters.field_of_view.left_degrees = 51.304901123046875;
-        
-        data
+        self.state.lock().unwrap().display_data.clone()
     }
 
     fn immediate_frame_data(&self, _near_z: f64, _far_z: f64) -> VRFrameData {
-        let mut data = VRFrameData::default();
-        // Position vector
-        data.pose.position = Some([0.5, -0.7, -0.3]);
-        // Orientation quaternion
-        // TODO: Add animation
-        data.pose.orientation = Some([0.9385081, -0.08066622, -0.3347714, 0.024972256]);
-
-        // Simulates HTC Vive projections
-        data.left_projection_matrix = [0.75620246, 0.0, 0.0, 0.0,
-                                       0.0, 0.68050665, 0.0, 0.0,
-                                      -0.05713458, -0.0021225351, -1.0000999, -1.0, 
-                                       0.0, 0.0, -0.10000999, 0.0];
-
-        data.left_view_matrix = [1.0, 0.0, 0.0, 0.0, 
-                                 0.0, 1.0, 0.0, 0.0, 
-                                 0.0, 0.0, 1.0, 0.0, 
-                                -0.035949998, 0.0, 0.015, 1.0];
-
-        data.right_projection_matrix = [0.75646526, 0.0, 0.0, 0.0, 
-                                        0.0, 0.68069947, 0.0, 0.0, 
-                                        0.055611316, -0.005315368, -1.0000999, -1.0, 
-                                        0.0, 0.0, -0.10000999, 0.0];
-
-        data.right_view_matrix = [1.0, 0.0, 0.0, 0.0,
-                                  0.0, 1.0, 0.0, 0.0,
-                                  0.0, 0.0, 1.0, 0.0,
-                                  0.035949998, 0.0, 0.015, 1.0];
-
-        data.timestamp = utils::timestamp();
-
-        data
+        self.state.lock().unwrap().frame_data.clone()
     }
 
     fn synced_frame_data(&self, near_z: f64, far_z: f64) -> VRFrameData {
@@ -167,6 +103,83 @@ impl MockVRState {
     pub fn handle_msg(&mut self, msg: MockVRControlMsg) {
         match msg {
 
+        }
+    }
+}
+
+impl MockVRState {
+    pub fn new(display_id: u32) -> Self {
+        let mut display_data = VRDisplayData::default();
+        
+        // Mock display data
+        // Simulates a virtual HTC Vive
+
+        display_data.display_name = "Mock VRDisplay".into();
+        display_data.display_id = display_id;
+        display_data.connected = true;
+
+        display_data.capabilities.can_present = true;
+        display_data.capabilities.has_orientation = true;
+        display_data.capabilities.has_external_display = true;
+        display_data.capabilities.has_position = true;
+
+        display_data.stage_parameters = Some(VRStageParameters {
+            sitting_to_standing_transform: [-0.9317312, 0.0, 0.36314875, 0.0, 0.0, 0.99999994, 0.0, 0.0, -0.36314875, 
+                                            0.0, -0.9317312, 0.0, 0.23767996, 1.6813644, 0.45370483, 1.0],
+            size_x: 2.0,
+            size_z: 2.0
+        });
+
+        display_data.left_eye_parameters.offset = [0.035949998, 0.0, 0.015];
+        display_data.left_eye_parameters.render_width = 1512;
+        display_data.left_eye_parameters.render_height = 1680;
+        display_data.left_eye_parameters.field_of_view.up_degrees = 55.82093048095703;
+        display_data.left_eye_parameters.field_of_view.right_degrees = 51.26948547363281;
+        display_data.left_eye_parameters.field_of_view.down_degrees = 55.707801818847656;
+        display_data.left_eye_parameters.field_of_view.left_degrees = 54.42263412475586;
+
+        display_data.right_eye_parameters.offset = [-0.035949998, 0.0, 0.015];
+        display_data.right_eye_parameters.render_width = 1512;
+        display_data.right_eye_parameters.render_height = 1680;
+        display_data.right_eye_parameters.field_of_view.up_degrees = 55.898048400878906;
+        display_data.right_eye_parameters.field_of_view.right_degrees = 54.37410354614258;
+        display_data.right_eye_parameters.field_of_view.down_degrees = 55.614715576171875;
+        display_data.right_eye_parameters.field_of_view.left_degrees = 51.304901123046875;
+        
+
+        let mut frame_data = VRFrameData::default();
+        // Position vector
+        frame_data.pose.position = Some([0.5, -0.7, -0.3]);
+        // Orientation quaternion
+        // TODO: Add animation
+        frame_data.pose.orientation = Some([0.9385081, -0.08066622, -0.3347714, 0.024972256]);
+
+        // Simulates HTC Vive projections
+        frame_data.left_projection_matrix = [0.75620246, 0.0, 0.0, 0.0,
+                                       0.0, 0.68050665, 0.0, 0.0,
+                                      -0.05713458, -0.0021225351, -1.0000999, -1.0, 
+                                       0.0, 0.0, -0.10000999, 0.0];
+
+        frame_data.left_view_matrix = [1.0, 0.0, 0.0, 0.0, 
+                                 0.0, 1.0, 0.0, 0.0, 
+                                 0.0, 0.0, 1.0, 0.0, 
+                                -0.035949998, 0.0, 0.015, 1.0];
+
+        frame_data.right_projection_matrix = [0.75646526, 0.0, 0.0, 0.0, 
+                                        0.0, 0.68069947, 0.0, 0.0, 
+                                        0.055611316, -0.005315368, -1.0000999, -1.0, 
+                                        0.0, 0.0, -0.10000999, 0.0];
+
+        frame_data.right_view_matrix = [1.0, 0.0, 0.0, 0.0,
+                                  0.0, 1.0, 0.0, 0.0,
+                                  0.0, 0.0, 1.0, 0.0,
+                                  0.035949998, 0.0, 0.015, 1.0];
+
+        frame_data.timestamp = utils::timestamp();
+
+        Self {
+            display_data,
+            frame_data
         }
     }
 }

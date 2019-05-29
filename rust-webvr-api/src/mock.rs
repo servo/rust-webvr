0199f69776077782c7e9cbd173d800112fbf48c2
Manish Goregaokar <manishsmail@gmail.com>
2019-05-29 15:38:05 -0700

    Support serialization for mock vr messages

--- rust-webvr-api/src/mock.rs
+++ rust-webvr-api/src/mock.rs
@@ -1,5 +1,7 @@
 use crate::{VREyeParameters, VRStageParameters};
 
+#[cfg_attr(feature = "serde-serialization", derive(Deserialize, Serialize))]
+#[derive(Debug)]
 pub enum MockVRControlMsg {
     SetViewerPose([f32; 3], [f32; 4]),
     SetEyeParameters(VREyeParameters, VREyeParameters),

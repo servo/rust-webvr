ad39d65a50aa6266c455d6053a6cd38c77a808e4
Manish Goregaokar <manishsmail@gmail.com>
2019-06-26 14:42:46 -0700

    Actually initialize things based on init values

--- rust-webvr/src/api/mock/display.rs
+++ rust-webvr/src/api/mock/display.rs
@@ -134,7 +134,7 @@ impl MockVRState {
 }
 
 impl MockVRState {
-    pub fn new(display_id: u32, _init: MockVRInit) -> Self {
+    pub fn new(display_id: u32, init: MockVRInit) -> Self {
         let mut display_data = VRDisplayData::default();
         
         // Mock display data
@@ -149,6 +149,7 @@ impl MockVRState {
         display_data.capabilities.has_external_display = true;
         display_data.capabilities.has_position = true;
 
+        // todo use init.eye_level
         display_data.stage_parameters = Some(VRStageParameters {
             sitting_to_standing_transform: [-0.9317312, 0.0, 0.36314875, 0.0, 0.0, 0.99999994, 0.0, 0.0, -0.36314875, 
                                             0.0, -0.9317312, 0.0, 0.23767996, 1.6813644, 0.45370483, 1.0],
@@ -156,7 +157,6 @@ impl MockVRState {
             size_z: 2.0
         });
 
-        display_data.left_eye_parameters.offset = [0.035949998, 0.0, 0.015];
         display_data.left_eye_parameters.render_width = 1512;
         display_data.left_eye_parameters.render_height = 1680;
         display_data.left_eye_parameters.field_of_view.up_degrees = 55.82093048095703;
@@ -164,7 +164,6 @@ impl MockVRState {
         display_data.left_eye_parameters.field_of_view.down_degrees = 55.707801818847656;
         display_data.left_eye_parameters.field_of_view.left_degrees = 54.42263412475586;
 
-        display_data.right_eye_parameters.offset = [-0.035949998, 0.0, 0.015];
         display_data.right_eye_parameters.render_width = 1512;
         display_data.right_eye_parameters.render_height = 1680;
         display_data.right_eye_parameters.field_of_view.up_degrees = 55.898048400878906;
@@ -174,32 +173,48 @@ impl MockVRState {
         
 
         let mut frame_data = VRFrameData::default();
-        // Position vector
-        frame_data.pose.position = Some([0.5, -0.7, -0.3]);
-        // Orientation quaternion
-        // TODO: Add animation
-        frame_data.pose.orientation = Some([0.9385081, -0.08066622, -0.3347714, 0.024972256]);
 
-        // Simulates HTC Vive projections
-        frame_data.left_projection_matrix = [0.75620246, 0.0, 0.0, 0.0,
-                                       0.0, 0.68050665, 0.0, 0.0,
-                                      -0.05713458, -0.0021225351, -1.0000999, -1.0, 
-                                       0.0, 0.0, -0.10000999, 0.0];
+        if let Some((position, orientation)) = init.viewer_origin {
+            frame_data.pose.position = Some(position);
+            frame_data.pose.orientation = Some(orientation);
+        } else {
+            // Position vector
+            frame_data.pose.position = Some([0.5, -0.7, -0.3]);
+            // Orientation quaternion
+            // TODO: Add animation
+            frame_data.pose.orientation = Some([0.9385081, -0.08066622, -0.3347714, 0.024972256]);
+        }
 
-        frame_data.left_view_matrix = [1.0, 0.0, 0.0, 0.0, 
-                                 0.0, 1.0, 0.0, 0.0, 
-                                 0.0, 0.0, 1.0, 0.0, 
-                                -0.035949998, 0.0, 0.015, 1.0];
+        if let Some((left, right)) = init.views {
+            display_data.left_eye_parameters.offset = left.offset;
+            display_data.right_eye_parameters.offset = right.offset;
+            frame_data.left_projection_matrix = left.projection;
+            frame_data.right_projection_matrix = right.projection;
+        } else {
+            display_data.left_eye_parameters.offset = [0.035949998, 0.0, 0.015];
+            display_data.right_eye_parameters.offset = [-0.035949998, 0.0, 0.015];
+            // Simulates HTC Vive projections
+            frame_data.left_projection_matrix = [0.75620246, 0.0, 0.0, 0.0,
+                                           0.0, 0.68050665, 0.0, 0.0,
+                                          -0.05713458, -0.0021225351, -1.0000999, -1.0, 
+                                           0.0, 0.0, -0.10000999, 0.0];
 
-        frame_data.right_projection_matrix = [0.75646526, 0.0, 0.0, 0.0, 
-                                        0.0, 0.68069947, 0.0, 0.0, 
-                                        0.055611316, -0.005315368, -1.0000999, -1.0, 
-                                        0.0, 0.0, -0.10000999, 0.0];
+            frame_data.left_view_matrix = [1.0, 0.0, 0.0, 0.0, 
+                                     0.0, 1.0, 0.0, 0.0, 
+                                     0.0, 0.0, 1.0, 0.0, 
+                                    -0.035949998, 0.0, 0.015, 1.0];
 
-        frame_data.right_view_matrix = [1.0, 0.0, 0.0, 0.0,
-                                  0.0, 1.0, 0.0, 0.0,
-                                  0.0, 0.0, 1.0, 0.0,
-                                  0.035949998, 0.0, 0.015, 1.0];
+            frame_data.right_projection_matrix = [0.75646526, 0.0, 0.0, 0.0, 
+                                            0.0, 0.68069947, 0.0, 0.0, 
+                                            0.055611316, -0.005315368, -1.0000999, -1.0, 
+                                            0.0, 0.0, -0.10000999, 0.0];
+
+            frame_data.right_view_matrix = [1.0, 0.0, 0.0, 0.0,
+                                      0.0, 1.0, 0.0, 0.0,
+                                      0.0, 0.0, 1.0, 0.0,
+                                      0.035949998, 0.0, 0.015, 1.0];
+        };
+
 
         frame_data.timestamp = utils::timestamp();
 
